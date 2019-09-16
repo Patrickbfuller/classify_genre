af8ca779957c913974ca7ba0e846deae0b8b5058
Patrick Fuller <patrickbfuller@gmail.com>
2019-09-15 18:30:38 -0700

    begin out collect genres function

--- genres.py
+++ genres.py
@@ -1,5 +1,25 @@
 import numpy as np
 import librosa
+import requests
+import bs4
+import youtube_dl
+from glob import glob
+import os
+import json
+
+ydl = youtube_dl.YoutubeDL(
+    params={
+        'format': 'bestaudio/best',
+        'ignoreerrors': True,
+        'outtmpl': '%(title)s.%(ext)s',
+        'playlistend': 1,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'm4a',
+            'preferredquality': '192',
+        }],
+    }
+)
 
 def extract_segment_features(y, sr):
     """
@@ -8,12 +28,12 @@ def extract_segment_features(y, sr):
     Input: An array of a audiofile.
 
     Output: Dictionary of segments with keys:
-        tempo, beats, chroma_stft, rmse, spec_cent, spec_bw, rolloff, zcr,
+        tempo, beats, chroma_stft, rms, spec_cent, spec_bw, rolloff, zcr,
         and mfcc values from 1-12. 
     """
     tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
     chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
-    rmse = librosa.feature.rmse(y=y)
+    rms = librosa.feature.rms(y=y)
     spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
     spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
     rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
@@ -21,17 +41,17 @@ def extract_segment_features(y, sr):
     mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=12)
     
     seg_features = {
-        'tempo' : tempo,
-        'beats' : beats.shape[0],
-        'chroma_stft' : chroma_stft.mean(),
-        'rmse' : rmse.mean(),
-        'spec_cent' : spec_cent.mean(),
-        'spec_bw' : spec_bw.mean(), 
-        'rolloff' : rolloff.mean(), 
-        'zcr' : zcr.mean()
+        "tempo" : tempo,
+        "beats" : beats.shape[0],
+        "chroma_stft" : chroma_stft.mean(),
+        "rms" : rms.mean(),
+        "spec_cent" : spec_cent.mean(),
+        "spec_bw" : spec_bw.mean(), 
+        "rolloff" : rolloff.mean(), 
+        "zcr" : zcr.mean()
     }
     seg_features.update(
-        {f'mfcc{i+1}': mfcc.mean() for i, mfcc in enumerate(mfccs)}
+        {f"mfcc{i+1}": mfcc.mean() for i, mfcc in enumerate(mfccs)}
     )
     return seg_features
 
@@ -62,10 +82,52 @@ def listen(fp:str, genre:str, duration=240, granularity=10):
         sr=None,
         duration=duration
     )
-
     song_rows = []
     for y in np.array_split(ts, duration/granularity):
         seg_features = {'song': fp, 'genre': genre}
         seg_features.update(extract_segment_features(y, sr))
         song_rows.append(seg_features)
     return song_rows
+
+#### Split to separate .py maybe?
+
+def get_urls(url:str):
+    """
+    Given a url for a youtube playlist, return a list of the urls for 
+    the videos in the playlist, using beautiful soup.
+    """
+    response = requests.get(url)
+    if response.status_code != 200:
+        return "RESPONSE_ERROR"
+    soup = bs4.BeautifulSoup(response.text, features="html5lib")
+    url_suffixes = [
+        a['href'] for a in soup.find_all('a', class_='pl-video-title-link')
+        ]
+    return ['https://www.youtube.com' + sfx for sfx in url_suffixes]
+
+def get_rows_from_m4a(url:str, genre:str):
+    """
+    Given the url of a youtube video and its genre, extract the m4a audio. For
+    unique segments of the audio, extract sonic features to dictionaries.
+    Remove the m4a and return a list of the dictionaries.
+    """
+    ydl.download([url])
+    m4a_fp = glob('*.m4a')[0]
+    song_rows = listen(m4a_fp, genre)
+    os.remove(m4a_fp)
+    return song_rows
+
+def collect_genre_features(genres:dict, data_fp="./data/genre_features.json"):
+    """
+    Given a dictionary with keys and values: name of genre and url of youtube
+    playlists corresponding to the genre, extract sonic features for each song
+    in segments. Store the features in json file with default as
+    'data/genre_features.json'
+    """
+    with open(data_fp, 'at+') as f:
+        for genre in genres:
+            urls = get_urls(genres[genre])
+            for url in urls:
+                song_rows = get_rows_from_m4a(url, genre)
+                for row in song_rows:
+                    f.write(row)
